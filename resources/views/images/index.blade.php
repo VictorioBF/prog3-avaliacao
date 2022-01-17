52dc1bdb1eb2f1384e5c945846f75ac02058225d
VictorioBF <victoriofaraco@gmail.com>
2022-01-17 20:28:52 -0300

    faltou um margin

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -5,7 +5,7 @@
 
 <div style="display: flex; width: 100%; padding-left: 10%; padding-right: 10%; flex-wrap: wrap;">
   @foreach($imgs as $img)
-  <div style="width: 20%; border: 1px solid #d4d4d4; border-radius: 10px; padding: 2%">
+  <div style="width: 20%; border: 1px solid #d4d4d4; border-radius: 10px; padding: 2%; margin: 10px">
     <a href="{{ route('images.show', $img) }}">
       <img src="{{asset('images/' . $img->image)}}" style="width: 100%;" />
     </a>
